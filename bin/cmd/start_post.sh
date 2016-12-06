7d9ad456907118bb08f8a3495e29c953aea65243
pproenca <pedro.proenca@olx.com>
2016-12-06 16:35:13 +0100

    Make ADB is available before killing the server

--- bin/cmd/start_post.sh
+++ bin/cmd/start_post.sh
@@ -6,7 +6,7 @@ fi
 athena.docker.wait_for_string_in_container_logs "$(athena.plugin.get_container_name)" "Appium REST http interface listener started"
 
 if athena.arg_exists "--adb-port"; then
-	if pgrep "adb" >/dev/null; then
+	if pgrep "adb" >/dev/null && hash adb 2>/dev/null; then
 		athena.info "Killing local ADB server..."
 		adb kill-server
 	fi
